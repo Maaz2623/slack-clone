42daaf063350e4002501870b607244c3176325ef
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-09-02 20:27:22 +0530

    Completed Forms Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import AuthScreen from '@/features/auth/components/auth-screen'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <AuthScreen />
+  )
+}
+
+export default HomePage
