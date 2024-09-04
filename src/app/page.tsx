7b3d956263967242fc0c9218821421babf541029
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-09-04 19:01:09 +0530

    Userbutton OCompleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 const HomePage = () => {
   const { signOut } = useAuthActions();
 
-  return <Button onClick={() => signOut()}>Sign out</Button>;
+  return <UserButton />;
 };
 
 export default HomePage;
